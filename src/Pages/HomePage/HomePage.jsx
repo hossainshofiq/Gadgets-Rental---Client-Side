e270a9d192d0b83fa59538ae4acf7bb3909a02e2
MZahidKamal <kamal.md.zahid@gmail.com>
2025-03-09 10:53:36 +0100

    Project setup completed using Vite React ReactRouter and TailwindCSS.

---
+++ src/Pages/HomePage/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import BannerComponent from "../../Components/BannerComponent/BannerComponent.jsx";
+import ProductsComponent from "../../Components/ProductsComponent/ProductsComponent.jsx";
+
+const HomePage = () => {
+    return (
+        <div>
+            <BannerComponent></BannerComponent>
+            <ProductsComponent></ProductsComponent>
+        </div>
+    );
+};
+
+export default HomePage;
