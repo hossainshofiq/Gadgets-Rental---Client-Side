90f6ec1579fbc4cba8ba0899f47ba9f31b6106d6
hossainshofiq <ah731710@gmail.com>
2025-03-12 01:34:43 +0600

    added a banner image

--- src/Components/BannerComponent/BannerComponent.jsx
+++ src/Components/BannerComponent/BannerComponent.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 const BannerComponent = () => {
     return (
-        <div>
-            BannerComponent
+        <div className='my-10'>
+            <h3 className='font-semibold text-xl my-3'>BannerComponent</h3>
+            <img src="https://cdn1.vectorstock.com/i/1000x1000/70/00/digital-gadgets-neon-banner-design-vector-25917000.jpg" alt="" />
+            
         </div>
     );
 };
